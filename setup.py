e1f4a8c0c385a000672dee12016226597169df21
Bas Wind <mailtobwind@gmail.com>
2018-12-17 17:41:25 +0100

    Update package to 'maxwell'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='mailtobwind+mac@gmail.com',
     description='Maxwell API Client',
     url='https://bitbucket.org/maxwell/maxwell-api-client',
-    packages=['maxwell-api-client'],
+    packages=['maxwell'],
     install_requires=open('requirements.txt', 'r').readlines(),
     include_package_data=True,
     long_description=open('README.md').read(),
