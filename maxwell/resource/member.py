622b04586ffd56ee66628c0fe0de6913c360d2a0
Bas Wind <mailtobwind@gmail.com>
2020-02-18 14:29:24 +0100

    Add Members

---
+++ maxwell/resource/member.py
@@ -0,0 +1,41 @@
+from maxwell.resource.base import Resource, ListResource
+
+
+class Member(Resource):
+    _depth = 1
+
+    def __init__(
+        self,
+        id=None,
+        first_name=None,
+        last_name=None,
+        client=None,
+        parent=None,
+        **kwargs,
+    ):
+        super().__init__(client, parent, id=id)
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+class Members(ListResource):
+    _path = "members"
+    _resource_class = Member
+    _depth = 2
+
+    def add(self, obj):
+        obj._client = self._client
+        obj._parent = self._parent
+        data = {"id": obj.id}
+        return self._request(
+            path=self._get_full_path(), method="post", data=data
+        )
+
+    def delete(self, obj):
+        obj._client = self._client
+        obj._parent = self._parent
+        data = {"id": obj.id}
+        return self._request(
+            path=self._get_full_path(), method="delete", data=data
+        )
--- maxwell/resource/team.py
+++ maxwell/resource/team.py
@@ -2,6 +2,7 @@ from maxwell.resource.base import Resource, ListResource
 from maxwell.resource.blueprint import Blueprints
 from maxwell.resource.channel import Channels
 from maxwell.resource.dashboard import Dashboards
+from maxwell.resource.member import Members
 
 
 class Team(Resource):
@@ -12,24 +13,31 @@ class Team(Resource):
         self._blueprints = None
         self._channels = None
         self._dashboards = None
+        self._members = None
 
     @property
     def Blueprints(self):
         if self._blueprints is None:
-            self._blueprints = Blueprints(self._client, self)
+            self._blueprints = Blueprints(client=self._client, parent=self)
         return self._blueprints
 
     @property
     def Channels(self):
         if self._channels is None:
-            self._channels = Channels(self._client, self)
+            self._channels = Channels(client=self._client, parent=self)
         return self._channels
 
     @property
     def Dashboards(self):
         if self._dashboards is None:
-            self._dashboards = Dashboards(self._client, self)
+            self._dashboards = Dashboards(client=self._client, parent=self)
         return self._dashboards
+
+    @property
+    def Members(self):
+        if self._members is None:
+            self._members = Members(client=self._client, parent=self)
+        return self._members
 
 
 class Teams(ListResource):
